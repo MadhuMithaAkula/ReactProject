25b76f1862bd296b53f0e2fd27b7c886f2ac9c5b
Madhu <akulamadhuri837@gmail.com>
2025-02-10 15:21:25 +0530

    welcome

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,9 +4,8 @@ function Home()
     <>
            <center><h1 className="text-danger">Home Page</h1></center> 
             <img 
-    src="public/Home.png" 
+    src="/Home.png" 
     alt="Not Found" 
-    className="public/Home.png" 
     style={{ width: "100%", height: "auto",maxWidth: "1500px"  }} 
 />
     </>
